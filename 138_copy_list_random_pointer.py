f3323999c58505f6ed87024ad7ec588118293439
Alice T <at4260@gmail.com>
2026-03-10 23:42:43 -0700

    problems

---
+++ 138_copy_list_random_pointer.py
@@ -0,0 +1,65 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # O(2n) -> O(n) time, O(n) space
+        # copy the first node, hold on next and random
+        dummy = Node(0)
+        og_pointer = head # pointer
+        new_pointer = dummy
+        old_to_new = {} # old node: new node
+
+        # first pass - creates the copy LL with val and next
+        # while saving the random node
+        while og_pointer:
+            new_pointer.next = Node(og_pointer.val)
+            new_pointer = new_pointer.next
+            old_to_new[og_pointer] = new_pointer
+            og_pointer = og_pointer.next
+
+        # second pass - traverse new LL and link up random
+        og_pointer = head # reset
+        new_pointer = dummy.next # reset
+        while new_pointer:
+            new_pointer.random = old_to_new[og_pointer.random] if og_pointer.random else None
+            og_pointer = og_pointer.next
+            new_pointer = new_pointer.next
+            
+
+        return dummy.next
+
+
+        # O(n) time, O(n) space
+        # copy the first node, hold on next and random
+        og_pointer = head # pointer
+        dummy = Node(0)
+        new_pointer = dummy
+        old_to_new = {None: None} # old node: new node
+
+        while og_pointer:
+            # lookup to connect to existing node or create new node
+            if og_pointer in old_to_new:
+                new_pointer.next = old_to_new[og_pointer]
+            else:
+                new_pointer.next = Node(og_pointer.val)
+                old_to_new[og_pointer] = new_pointer.next
+            new_pointer = new_pointer.next
+
+            if og_pointer.random in old_to_new:
+                # make the .random link
+                new_pointer.random = old_to_new[og_pointer.random]
+            else:
+                # create the node
+                new_random_node = Node(og_pointer.random.val) # None already handled above
+                old_to_new[og_pointer.random] = new_random_node
+                new_pointer.random = new_random_node
+            og_pointer = og_pointer.next
+
+        return dummy.next
---
+++ 143_reorder_list.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # O(3*n/2) => O(n) time, O(1) space
+        # fast/slow pointers to get to the middle and end of LL
+        fast, slow = head, head
+        # odd len - true middle; even len - left of middle
+        while fast and fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # use slow pointer to reverse right half of LL
+        # don't let right half reverse back into the left half
+        # ex: in [1,2,3,4] -> [1,2] and [4,3] not [4,3,2] which causes a 
+        # bug in the relink later
+        
+        # sever middle node's link
+        mid = slow         
+        slow = slow.next   
+        mid.next = None
+        # sever next node's link back to the middle
+        prev = None
+
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+            
+
+        # relink using head and end pointers while moving inwards
+        left = head
+        right = prev
+        while left and right:
+            tmp_left = left.next
+            left.next = right
+            left = tmp_left
+            
+            tmp_right = right.next
+            right.next = left
+            right = tmp_right
+
